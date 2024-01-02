d09aa1d63330e84bb5743a0f7b45472066a96ada
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-01-02 17:50:51 +0100

    feat: Minor changes

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -62,16 +62,15 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <div className="flex h-8 items-end space-x-1"
+        <div
+          className="flex h-8 items-end space-x-1"
           aria-live="polite"
           aria-atomic="true"
         >
           {errorMessage && (
             <>
-               <ExclamationCircleIcon className='h-5 w-5 text-red-500'/>
-               <p className='text-sm text-red-500'>
-                 {errorMessage}
-                </p>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
         </div>
@@ -82,7 +81,7 @@ export default function LoginForm() {
 
 function LoginButton() {
   const { pending } = useFormStatus();
-
+ 
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
